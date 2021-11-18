b7456dc8b0d988528644f570d010e824b77db6e2
meiji163 <mysatellite99@gmail.com>
2021-11-17 20:54:05 -0800

    token minting

--- ecoshift/contracts/EcoWallet.sol
+++ ecoshift/contracts/EcoWallet.sol
@@ -1,15 +1,18 @@
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.8.7;
 
-import "@openzeppelin/contracts/payment/PaymentSplitter.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./APIConsumer.sol"
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./APIConsumer.sol";
 
 contract EcoWallet is APIConsumer, PaymentSplitter, ERC721, Ownable{
     uint256 public tokenCounter;
 
-    mapping(string => string) public tierToWalletURI;
+    mapping(uint8 => string) public tierToWalletURI;
+    mapping(uint256 => address) public tokenIdToOwner;
+    mapping(uint256 => uint256) public tokenIdToBalance;
 
-    constructor public (
+    constructor (
             address[] memory _payees, 
             uint256[] memory _shares,
             address _oracle,
@@ -22,17 +25,43 @@ contract EcoWallet is APIConsumer, PaymentSplitter, ERC721, Ownable{
         ERC721("EcoWallet", "ECO")
     {
         tokenCounter = 0;
+        tierToWalletURI[1] = "URI1"; // grunt tier environmental activist   .01 ETH
+        tierToWalletURI[2] = "URI2"; // warrior tier vegan recyclist        .1  ETH
+        tierToWalletURI[3] = "URI3"; // commander tier activist mobilizer   .5  ETH
+        tierToWalletURI[4] = "URI4"; // emperor tier global warming sage    1   ETH
+        tierToWalletURI[5] = "URI5"; // God tier woke climate legend        5   ETH
     }
 
-    function tokenURI(uint256 tokenId) public view override (ERC721) returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override (ERC721) returns (string memory){
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        //TODO
-        return "THE URI"
+        return tierToWalletURI[ getTier(tokenId)];
+    }
+
+    function getTier(uint256 tokenId) public view returns (uint8) {
+        require(tokenIdToOwner[tokenId] != address(0x0), "EcoWallet Tier: nonexistent token");
+        
+        // determine the tiers
+        // 1 ETH = 1000000000000000000 wei
+        uint8 tier;
+        if (tokenIdToBalance[tokenId] < 10000000000000000){
+            tier = 1;
+        } else if (tokenIdToBalance[tokenId] < 100000000000000000){
+            tier = 2;
+        } else if (tokenIdToBalance[tokenId] < 500000000000000000){
+            tier = 3;
+        } else if (tokenIdToBalance[tokenId] < 1000000000000000000){
+            tier = 4;
+        } else if (tokenIdToBalance[tokenId] < 5000000000000000000){
+            tier = 5; //UR A GODD
+        }
+        return tier;
     }
 
     function createWallet() public onlyOwner{
-        _safeMint(msg.sender, tokenCounter)
+        _safeMint(msg.sender, tokenCounter);
+        tokenIdToOwner[tokenCounter] = msg.sender;
+        tokenIdToBalance[tokenCounter]= 0;
         tokenCounter = tokenCounter+1;
     }
 
@@ -40,5 +69,3 @@ contract EcoWallet is APIConsumer, PaymentSplitter, ERC721, Ownable{
         //TODO
     }
 }
-
-
