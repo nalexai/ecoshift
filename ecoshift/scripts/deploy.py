9d80e1326b84c07c4eaf262d0470d31641b43ee0
sourface94 <nathan_jf@hotmail.co.uk>
2021-11-21 12:54:53 +0000

    feat: deploying of ecowallet

--- ecoshift/scripts/deploy.py
+++ ecoshift/scripts/deploy.py
@@ -3,27 +3,32 @@ from brownie import APIConsumer, config, network, EcoWallet
 from web3 import Web3
 from scripts.utils import get_account, get_contract, JOB_IDS
 
-def deploy_api_consumer():
-    #jobId = config["networks"][network.show_active()]["jobId"]
 
-    #jobId = JOB_IDS[network.show_active()]["GET"]["uint256"]
+def deploy_api_consumer():
+    # jobId = config["networks"][network.show_active()]["jobId"]
+
+    # jobId = JOB_IDS[network.show_active()]["GET"]["uint256"]
     jobId = "d5270d1c311941d0b08bead21fea7747"
     fee = config["networks"][network.show_active()]["fee"]
     account = get_account()
-    oracle = "0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8" #get_contract("oracle").address
+    oracle = (
+        "0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8"  # get_contract("oracle").address
+    )
     link_token = get_contract("link_token").address
     api_consumer = APIConsumer.deploy(
-            oracle,
-            Web3.toHex(text=jobId),
-            fee,
-            link_token,
-            {"from": account},
-            publish_source=False  # config["networks"][network.show_active()].get("verify", False),
+        oracle,
+        Web3.toHex(text=jobId),
+        fee,
+        link_token,
+        {"from": account},
+        publish_source=False,  # config["networks"][network.show_active()].get("verify", False),
     )
     print(f"API Consumer deployed to {api_consumer.address}")
     return api_consumer
 
+
 def deploy_ecowallet():
+    print("hello")
     jobId = config["networks"][network.show_active()]["jobId"]
     fee = config["networks"][network.show_active()]["fee"]
     account = get_account()
@@ -31,14 +36,15 @@ def deploy_ecowallet():
     link_token = get_contract("link_token").address
 
     ecowallet = EcoWallet.deploy(
-            oracle,
-            fee,
-            Web3.toHex(text=jobId),
-            link_token,
-            {"from": account},
-            publish_source=False  # config["networks"][network.show_active()].get("verify", False),
+        oracle,
+        fee,
+        Web3.toHex(text=jobId),
+        link_token,
+        {"from": account},
+        publish_source=False,  # config["networks"][network.show_active()].get("verify", False),
     )
     return ecowallet
+
 
 def main():
     deploy_ecowallet()
